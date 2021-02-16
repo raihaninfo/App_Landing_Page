3302c67aef0b1c71837d0883c0830fcf89f36adf
Md Abu. Raihan <raihanmahmudi35@gmail.com>
2021-02-16 11:08:42 +0600

    app landing page create

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    // navbar sharink
+    $(window).on("scroll",function(){
+        if($(this).scrollTop()>90){
+            $(".navbar").addClass("navbar-shrink");
+        }
+        else{
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    });
+
+
+
+    // popup video
+    const videoSrc = $("#plear-1").attr("src");
+    $(".video-play-btn, .video-popup").on("click", function(){
+        if($(".video-popup").hasClass("open")){
+            $(".video-popup").removeClass("open");
+            $("#plear-1").attr("src", "")
+        }
+        else{
+            $(".video-popup").addClass("open");
+            if($("#plear-1").attr("src")==''){
+                $("#plear-1").attr("src", videoSrc);
+            }
+        }
+    });
+            // owl carousel
+    $('.feature-corosel').owlCarousel({
+        loop:true,
+        margin:0,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+            }
+        });
+});
