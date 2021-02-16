01422574318c7bcb08e6690cf665d533783592d9
Md Abu. Raihan <raihanmahmudi35@gmail.com>
2021-02-16 23:41:28 +0600

    testimonials section complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,4 +61,22 @@ $(document).ready(function () {
             }
             }
     });
+    // testimonials carousel
+    $('.testimonials-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+            }
+    });
 });
