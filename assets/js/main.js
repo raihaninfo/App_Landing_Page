703f8a895176da38f99604a7c5d0ce6b6067cddd
Md Abu. Raihan <raihanmahmudi35@gmail.com>
2021-02-17 15:34:18 +0600

    bug fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,4 +79,22 @@ $(document).ready(function () {
             }
             }
     });
+    // team carousel
+    $('.team-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+            }
+    });
 });
