7094d0927d177101bd07c813ce874d2f5b5e14fe
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-02-19 07:33:42 +0600

    dark theme complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,4 +105,41 @@ $(document).ready(function () {
     $(".nav-link").on("click", function(){
         $(".navbar-collapse").collapse("hide");
     });
+
+    // toggle theme -dark and light theme
+    function toggleTheme(){
+        if(localStorage.getItem("shala-theme") !== null){
+            if(localStorage.getItem("shala-theme") == "dark"){
+                $("body").addClass("dark");
+            }
+            else{
+                $("body").removeClass("dark");
+            }
+        }
+        updateIcon();
+    }
+    toggleTheme();
+
+    $(".toggle-theme").on("click", function(){
+        $("body").toggleClass("dark");
+        if($("body").hasClass("dark")){
+            localStorage.setItem("shala-theme", "dark");
+        }
+        else{
+            localStorage.setItem("shala-theme", "light");
+        }
+        updateIcon();
+    })
+    function updateIcon(){
+        if($("body").hasClass("dark")){
+            $(".toggle-theme i").removeClass("fa-moon");
+            $(".toggle-theme i").addClass("fa-sun")
+        }
+        else{
+            $(".toggle-theme i").removeClass("fa-sun");
+            $(".toggle-theme i").addClass("fa-moon")
+        }
+    }
+
+
 });
